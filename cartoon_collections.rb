62356655356c769a0ebf6590d18bc0c11948a053
Max Petersen <mglxp5@gmail.com>
2018-09-04 21:47:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,15 @@ def summon_captain_planet(calls)# code an argument here
   end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.each do |call|
+    if call.length > 4
+      return TRUE
+    else
+      return FALSE
+    end
+  end
 end
 
 def find_the_cheese# code an argument here
